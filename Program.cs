55d9a9b7fd0d36c67b4a09ec624cf56647616ff2
malil6384 <malil6384@19PG0Q2>
2022-09-21 12:58:31 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace WhileLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[10];
+            int i = 0;
+            while(i < nums.Length)
+            {
+                nums[i] = i;
+                Console.Write(" | " + nums[i]);
+                i++;
+            }
+            Console.Write("\n\n");
+            do
+            {   
+                i--;
+                if (i == 8) { Console.Write(" | Skippped");continue;}
+                if(i == 3) { Console.Write(" | Done"); break;}
+                Console.Write(" | " + nums[i]);
+            }
+            while(i > 0);
+            Console.ReadKey();
+
+        }
+    }
+}
